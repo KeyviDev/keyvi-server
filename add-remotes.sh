04b9b5937cfbccd8825149f3379e70b85aeb1c93
Hendrik Muhs <hendrik.muhs@gmail.com>
2019-03-06 19:52:48 +0100

    add remotes and 3rdparty pull script

---
+++ add-remotes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -ev
+
+git remote add keyvi        git@github.com:KeyviDev/keyvi.git
+git remote add upstream     git@github.com:KeyviDev/keyvi-server.git
+git remote add brpc         git@github.com:apache/incubator-brpc.git
