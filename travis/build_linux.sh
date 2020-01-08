0cf192c95e8ac8b42060e175821c74615f2bd52e
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-01-08 23:05:14 +0100

    try 7.3 on osx

--- travis/build_linux.sh
+++ travis/build_linux.sh
@@ -4,6 +4,7 @@ set -ex
 cd /io
 
 # TODO: move into the docker image
+apt-get update
 apt-get install libssl-dev libgflags-dev libprotobuf-dev libprotoc-dev protobuf-compiler libleveldb-dev
 mkdir build
 cd build
