ea91cb5fad943e2dffa23dbb8c07d7d1ff369f98
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-03-29 14:43:34 +0200

    remove installing dependencies after moving it into docker

--- travis/build_linux.sh
+++ travis/build_linux.sh
@@ -3,9 +3,6 @@ set -ex
 
 cd /io
 
-# TODO: move into the docker image
-apt-get update
-apt-get install -y libssl-dev libgflags-dev libprotobuf-dev libprotoc-dev protobuf-compiler libleveldb-dev libgoogle-perftools-dev
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=$CONF ..
