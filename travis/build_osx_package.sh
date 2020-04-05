bb4437599f589357cf3873992bc7bbbf6a4f7b47
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-04-05 11:54:49 +0200

    remove installing dependencies after moving it into docker (#8)
    remove installing dependencies after moving it into docker, fix OSX build

--- travis/build_osx_package.sh
+++ travis/build_osx_package.sh
@@ -4,7 +4,7 @@ set -x
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=release -DZLIB_ROOT=/usr/local/opt/zlib ..
-make -j 4
+make -j 4 || travis_terminate 1;
 
 #cd ..
 
--- travis/setup_osx.sh
+++ travis/setup_osx.sh
@@ -4,9 +4,18 @@ set -x
 diskutil erasevolume HFS+ 'ram-disk' `hdiutil attach -nomount ram://6165430`
 df -h
 
+brew update
+brew install zlib
+brew install snappy
+brew install openssl git gnu-getopt coreutils
+brew install gflags protobuf leveldb
+brew install gperftools
+
+brew upgrade pyenv
+
 export PATH="${HOME}/.pyenv/shims/:/root/.pyenv/bin:${PATH}"
 
-pyenv install ${PYTHON_VERSION}
-pyenv global ${PYTHON_VERSION}
+pyenv install ${PYTHON_VERSION} || travis_terminate 1;
+pyenv global ${PYTHON_VERSION} || travis_terminate 1;
 
-pip install -r python/requirements.txt
+pip install -r python/requirements.txt || travis_terminate 1;
