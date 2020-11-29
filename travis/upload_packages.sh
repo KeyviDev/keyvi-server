268baef4a01d787b78545312b244d10ce2a97864
Hendrik Muhs <hendrik.muhs@gmail.com>
2020-11-29 14:02:38 +0100

    remove travis

--- travis/upload_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-pip install --user wheel twine --upgrade
-
-cd python
-if [ -n "$(ls -A wheelhouse)" ]; then
-    twine upload --config-file ../travis/pypirc -u $PYPI_USERNAME -p $PYPI_PASSWORD wheelhouse/*
-fi
